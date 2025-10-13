eccb3f16774ab136e0da961510d8ffd9058e1356
janabakri <bkeyh382@gmail.com>
2025-10-13 09:00:21 +0000

    add

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to n and prints whether
+ * it is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -14,3 +26,4 @@ int main(void)
 
 	return (0);
 }
+
