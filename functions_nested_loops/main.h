9ec8c917ee381daebf4aefd652b672efb40bbb48
janabakri <bkeyh382@gmail.com>
2025-10-14 08:27:20 +0000

    add

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 #endif /* MAIN_H */
