d45e78d40ce234e7a0fd93ec24fe829e047f4bdc
janabakri <bkeyh382@gmail.com>
2025-10-21 07:52:32 +0000

    add

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,32 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string,
- * starting with the first character, followed by a new line.
- * @str: input string
- *
- * Description: Only allowed functions are write, malloc, free.
- */
+* puts2 - prints every other character of a string,
+* starting with the first character
+* @str: input string
+*
+* Description: This function prints only the characters
+* whose index numbers are even (0, 2, 4, ...).
+* The odd-indexed characters are skipped using an else statement.
+* After finishing, a newline character is printed.
+*
+* Return: void
+*/
 void puts2(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	if (str == NULL)
-		return;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+else
+{
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
-	_putchar('\n');
+}
+i++;
+}
+_putchar('\n');
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,5 @@
 
 int _putchar(char c);
 void puts2(char *str);
-void puts_half(char *str);
 
 #endif /* MAIN_H */
