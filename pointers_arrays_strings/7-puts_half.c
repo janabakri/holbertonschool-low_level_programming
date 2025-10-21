8724a7f111256931e760b733fad41976a10d4c00
janabakri <bkeyh382@gmail.com>
2025-10-21 08:04:24 +0000

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,30 +2,34 @@
 
 /**
  * puts_half - prints the second half of a string, followed by a new line
- * @str: input string
+ * @str: the input string
  */
 void puts_half(char *str)
 {
 	int len = 0, start;
 
-	if (str == NULL)
-		return;
-
 	/* حساب طول السلسلة */
 	while (str[len] != '\0')
+	{
 		len++;
+	}
 
-	/* حساب نقطة بداية النصف الثاني حسب طول السلسلة */
+	/* تحديد نقطة البداية للنصف الثاني */
 	if (len % 2 == 0)
+	{
 		start = len / 2;
+	}
 	else
-		start = (len + 1) / 2;
+	{
+		start = (len - 1) / 2 + 1;
+	}
 
-	/* طباعة من بداية النصف الثاني حتى نهاية السلسلة */
+	/* الطباعة من منتصف السلسلة للنهاية */
 	while (str[start] != '\0')
 	{
 		_putchar(str[start]);
 		start++;
 	}
+
 	_putchar('\n');
 }
