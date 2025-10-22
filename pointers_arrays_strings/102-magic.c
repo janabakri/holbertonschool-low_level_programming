db1a1ba31de17a522a2e073f02fbdd25319e9066
janabakri <bkeyh82@gmail3.com>
2025-10-22 14:38:01 +0000

    update

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -24,7 +24,7 @@ int main(void)
      * - do not remove anything from the initial code (not even the comments)
      * - you are allowed to use the standard library
      */
-    *(p + 5) = 98;
+    *(&n + 5) = 98;
     /* ...so that this prints 98\n */
     printf("a[2] = %d\n", a[2]);
     return (0);
