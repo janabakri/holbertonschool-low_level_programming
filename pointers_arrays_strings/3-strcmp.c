65f9333d46fa593edb8747370a499017ac54e8f3
janabakri <bkeyh382@gmail.com>
2025-10-22 07:29:52 +0000

    update

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,15 +6,15 @@
  * @s2: Second string
  *
  * Return: An integer less than, equal to, or greater than 0
- *         if s1 is found, respectively, to be less than,
- *         to match, or be greater than s2.
+ * if s1 is found, respectively, to be less than,
+ * to match, or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
-    return (*s1 - *s2);
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
