05b3d123891ef51d431b47f4b58eaa02b3f9299f
janabakri <bkeyh382@gmail.com>
2025-10-27 08:29:26 +0000

    add

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,14 +1,32 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-char *_memset(char *s, char b, unsigned int n);
+#include <stdio.h>
+
+/* Function prototypes */
+
+/* 1. memcpy */
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* 2. strchr */
 char *_strchr(char *s, char c);
+
+/* 3. strspn */
 unsigned int _strspn(char *s, char *accept);
+
+/* 4. strpbrk */
 char *_strpbrk(char *s, char *accept);
+
+/* 5. strstr */
 char *_strstr(char *haystack, char *needle);
+
+/* 6. Chessboard */
+void print_chessboard(char (*a)[8]);
+
+/* 7. Sum of diagonals */
+void print_diagsums(int *a, int size);
+
+/* _putchar function used in print_chessboard */
+int _putchar(char c);
 
 #endif /* MAIN_H */
