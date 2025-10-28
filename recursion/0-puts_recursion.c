7dba11cf576977e5ea85bbd0bf742c38ffde0784
janabakri <bkeyh382@gmail.com>
2025-10-28 07:21:51 +0000

    fix

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,12 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')  /* base case: if we reach the end of the string */
-    {
-        _putchar('\n');  /* print newline at the end */
-        return;          /* stop recursion */
-    }
-
-    _putchar(*s);         /* print current character */
-    _puts_recursion(s + 1); /* recursive call for the next character */
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
