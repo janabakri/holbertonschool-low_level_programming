b807afb7d712dcd212dc56f39eb16d74808e196e
janabakri <bkeyh382@gmail.com>
2025-11-04 09:51:43 +0000

    fix

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * _isdigit - checks if a string contains only digits
- * @s: string to check
+ * _isdigit - Checks if a string contains only digits
+ * @s: The string to check
  *
  * Return: 1 if all characters are digits, 0 otherwise
  */
@@ -22,10 +22,10 @@ int _isdigit(char *s)
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string
+ * _strlen - Returns the length of a string
+ * @s: The string
  *
- * Return: length of string
+ * Return: The length of the string
  */
 int _strlen(char *s)
 {
@@ -37,7 +37,7 @@ int _strlen(char *s)
 }
 
 /**
- * errors - prints "Error" and exits with status 98
+ * errors - Prints "Error" and exits with status 98
  */
 void errors(void)
 {
@@ -46,16 +46,16 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Multiplies two positive numbers
+ * @argc: The number of arguments
+ * @argv: The array of argument strings
  *
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, n1, n2, *result, a = 0;
+	int len1, len2, len, i, carry, n1, n2, *result, start = 0;
 
 	if (argc != 3)
 		errors();
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 
 	result = malloc(sizeof(int) * len);
 	if (result == NULL)
-		return (1);
+		errors();
 
 	for (i = 0; i < len; i++)
 		result[i] = 0;
@@ -81,7 +81,6 @@ int main(int argc, char *argv[])
 	{
 		n1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			n2 = s2[len2] - '0';
@@ -89,7 +88,6 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
@@ -97,15 +95,15 @@ int main(int argc, char *argv[])
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
-			a = 1;
-		if (a)
+			start = 1;
+		if (start)
 			_putchar(result[i] + '0');
 	}
 
-	if (!a)
+	if (!start)
 		_putchar('0');
-
 	_putchar('\n');
+
 	free(result);
 	return (0);
 }
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -8,4 +8,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+/* Prototypes used in 101-mul.c */
+int _isdigit(char *s);
+int _strlen(char *s);
+void errors(void);
+
 #endif /* MAIN_H */
