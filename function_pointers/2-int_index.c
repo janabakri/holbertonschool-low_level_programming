233daddc3e9d5fe41e8f0c688092a8420328ca5e
janabakri <you@example.com>
2025-11-10 08:48:45 +0000

    add

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,4 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-#include <stddef.h> /* for size_t */
+#include "function_pointers.h"
 
 /**
  * int_index - searches for an integer in an array
@@ -12,6 +9,18 @@
  * Return: index of first element for which cmp does not return 0,
  *         -1 if no element matches or size <= 0
  */
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
 
-#endif /* FUNCTION_POINTERS_H */
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
