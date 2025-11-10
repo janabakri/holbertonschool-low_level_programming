d467921260c2e45eb80592994b6be0a8b7cfe2ef
janabakri <you@example.com>
2025-11-10 08:44:55 +0000

    fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,4 +1,3 @@
-#include <stddef.h> /* for size_t and NULL */
 #include "function_pointers.h"
 
 /**
@@ -11,11 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL || action == NULL)
-        return;
+	if (array == NULL || action == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
